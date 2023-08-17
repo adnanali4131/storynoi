bbead3d84af4b522f8cd4830d52fd12c0b7e719b
Abdul Wahab <abdulwahabdev0@gmail.com>
2023-08-17 18:21:15 +0500

    pdf only text

--- pages/api/generate-pdf/index.js
+++ pages/api/generate-pdf/index.js
@@ -4,35 +4,43 @@ export default async function handler(req, res) {
   const { storyContent } = JSON.parse(req.body);
   const browser = await puppeteer.launch();
   try {
-    const pageContent = storyContent.map((content) => {
-      return content.image.length > 0
-        ? `<div
-          key={${content.heading}}
+    let pageContent = "";
+
+    for (let i = 0; i < storyContent.length; i++) {
+      pageContent +=
+        storyContent[i].image.length > 0
+          ? `<div
+          key={${storyContent[i].heading}}
           style="display:flex; height:100%; width:100%; justify-content:center; align-items:center; flex-direction:column; page-break-after:always"
         >
-          <img src={${content.image}} style="width:1024px; height:1024px" />
+          <img src="${storyContent[i].image}" alt="${storyContent[i].heading}" style="width:100px, height:100px" />
         </div>
         <div
-      key={${content.heading}}
+      key={${storyContent[i].heading}}
       style="display:flex; height:100%; width:100%; justify-content:center; align-items:center; flex-direction:column; page-break-after:always"
     >
-      <h1>${content.heading}</h1>
-      <p>${content.description}</p>
+    <div style="width:75%;justify-content:center; align-items:center; flex-direction:column; display:flex;">
+    <h1>${storyContent[i].heading}</h1>
+    <p style="text-align:center">${storyContent[i].description}</p>
+    </div>
     </div>
         `
-        : `<div
-      key={${content.heading}}
+          : `<div
+      key={${storyContent[i].heading}}
       style="display:flex; height:100%; width:100%; justify-content:center; align-items:center; flex-direction:column; page-break-after:always"
     >
-      <h1>${content.heading}</h1>
-      <p>${content.description}</p>
+      <div style="width:75%;justify-content:center; align-items:center; flex-direction:column; display:flex;">
+      <h1>${storyContent[i].heading}</h1>
+      <p style="text-align:center">${storyContent[i].description}</p>
+      </div>
     </div>`;
-    });
+    }
+
     const pdfBufferArray = [];
 
     const page = await browser.newPage();
-
-    await page.setContent(pageContent.join(" "));
+    await page.goto("about:blank");
+    await page.setContent(pageContent);
 
     const pdfBuffer = await page.pdf({
       format: "A4",

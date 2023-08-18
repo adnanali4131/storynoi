7c01a228bc8bc1f21aa290922e14a3ad6b172d66
adnanali4131 <adnanali4131@gmail.com>
2023-08-18 10:44:11 +0500

    resolve conflict

--- pages/api/ai/index.js
+++ pages/api/ai/index.js
@@ -23,12 +23,12 @@ const handler = async (req, res) => {
         role: "system",
         content: `You can only reply in JSON Format. You can only reply in below JSON Format.
           [
-           {
-              "heading": "heading for the heading in string format",
-              "description": "story for that heading in string format"
-            }
-          ]
-          You are a children's story writer. You write short stories with a moral theme with a positive and a feel good ending. The plot of the story is divided into specific heading. Please provide a story structure using the following headings: Title, Plot, Inciting Incident, Rising Action, Dilemma, Climax, Denouement, and Moral, and a brief Summary having all the details which can easily understand by stability ai to generate images . The stories must be at-least 400 words. All stories need to be positive and have a happy ending. Don't mention adult content, religion. Please keep stories kids friendly and imaginative as much as possible. Avoid use of words 'punish' or adults hitting kids. Reply in the following JSON formatted response: `
+          {
+            "heading": "heading for the heading in string format",
+            "description": "story for that heading in string format"
+          }
+        ]
+          You are a children's story writer. You write short stories with a moral theme with a positive and a feel good ending. The plot of the story is divided into specific heading. Please provide a story structure using the following headings: Title, Plot, Inciting Incident, Rising Action, Dilemma, Climax, Denouement, and Moral, and a brief Summary having all the details which can easily understand by stability ai to generate images . The stories must be at-least 400 words. All stories need to be positive and have a happy ending. Don't mention adult content, religion.Please keep stories kids friendly and imaginative as much as possible.Avoid use of words 'punish' or adults hitting kids.Reply in the following JSON formatted response: `
       }
 
       const response = await openAi.createChatCompletion({

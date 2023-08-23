6ec581c64e3876ac7ca635132058aefb921c6df8
adnanali4131 <adnanali4131@gmail.com>
2023-08-23 11:15:05 +0500

    update UI

--- components/landing/discussions.js
+++ components/landing/discussions.js
@@ -9,13 +9,13 @@ const DiscussionsComponent = ({ discussion, imageUrls }) => {
 
   return (
     <div key={discussion?.heading} className="grid grid-cols-2 gap-5 text-black bg-white shadow-sm rounded-xl">
-      <div className='border-r-[2px]'>
-        <div className="flex-1 p-4 flex flex-col justify-center">
+      <div >
+        <div className="flex-1 p-4 flex flex-col ">
           <h2 className="text-[24px] font-semibold text-center">Discussions</h2>
           <ul>
             {Array.isArray(discussion?.description) &&
               discussion?.description?.map((discuss, index) => (
-                <li key={index} className="text-lg leading-7 text-center my-6">
+                <li key={index} className="text-lg leading-7  my-6">
                   {discuss}
                   <span className='border-span my-8 '></span>
                   <span className="border-span"></span>
@@ -25,14 +25,18 @@ const DiscussionsComponent = ({ discussion, imageUrls }) => {
           </ul>
         </div>
       </div>
-      <div className='bg-[#F8F8F8]'>
-        <div className="w-full flex justify-center text-center overflow-hidden shadow-[10px 4px 12px 0px #0000001F] d-flex align-items-center justify-content-center" style={{ height: '400px' }}>
+      <div >
+        <div className="w-full flex justify-center text-center overflow-hidden shadow-[10px 4px 12px 0px #0000001F] d-flex align-items-center justify-content-center" style={{ height: '600px' }}>
           {!hasValidImages ? (
-            <Image
-              src={downloadImage}
-              alt="download"
-              className='mt-60'
-            />
+            <div className=' h-[400px]'>
+              <h2 className="text-[24px] pt-4 font-semibold text-center">Coloring activity</h2>
+              <Image
+                src={downloadImage}
+                alt="download"
+                className='mt-40'
+              />
+              <p>No image generated yet!</p>
+            </div>
           ) : (
             <ImageSlider images={imageUrls} />
           )}

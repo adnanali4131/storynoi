91ce27de0446d5a7982584a168d4d176b762a433
adnanali4131 <adnanali4131@gmail.com>
2023-08-24 16:58:12 +0500

    velidation sechema

--- components/forgot/Forgot.js
+++ components/forgot/Forgot.js
@@ -3,8 +3,20 @@
 import Image from "next/image";
 import User from "@/assets/auth/icons/user.svg";
 import forgotIcon from "@/assets/auth/forgot/forgotlock.svg"
+import { emailSchema } from "@/assets/yup/schema";
+import { useFormik } from "formik";
 
 const Forgot = ({ width }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+    validationSchema: emailSchema,
+    onSubmit: (values) => {
+      console.log('Email:', values.email);
+
+    },
+  });
   return (
     <div className="relative px-10 py-12 bg-white  rounded-xl" style={{ width }}>
       <div className="text-[30px] ">
@@ -17,23 +29,33 @@ const Forgot = ({ width }) => {
         <span className="flex items-center justify-center">
           <p className="text-[14px]">Don’t worry we got you covered. Enter the email <br></br> address associated with this account.</p>
         </span>
-        <div className="flex flex-col gap-5 mt-10">
-          <div className="flex items-center justify-between my-4 gap-3 px-4 overflow-hidden bg-white border rounded-xl custom-input">
-            <div className="flex w-[90%] gap-3">
-              <Image src={User} width={15} alt="user-icon" />
-              <input
-                type="email"
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                className="py-3 outline-none text-[16px] w-full"
-              />
+        <form onSubmit={formik.handleSubmit}>
+          <div className="flex flex-col gap-5 mt-10">
+            <div className="flex items-center justify-between  gap-3 px-4 overflow-hidden bg-white border rounded-xl custom-input">
+              <div className="flex w-[90%] ">
+                <Image src={User} width={15} alt="user-icon" />
+                <input
+                  type="email"
+                  placeholder="Email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  name="email"
+                  className="py-4 outline-none text-[16px] w-full"
+                />
+              </div>
             </div>
+
+            {formik.touched.email && formik.errors.email ? (
+              <div className="text-red-500 text-[14px] mt-[-12px]">
+                {formik.errors.email}
+              </div>
+            ) : null}
+            <button type="submit" className="text-white bg-dark-orange rounded-xl text-[16px] p-3 mt-4">
+              Submit
+            </button>
           </div>
-          <button className="text-white bg-dark-orange rounded-xl text-[16px] p-3" >
-            Submit
-          </button>
-        </div>
+        </form>
       </div>
     </div>
   );
--- components/resetPassword/ResetPassword.js
+++ components/resetPassword/ResetPassword.js
@@ -4,9 +4,23 @@ import Image from "next/image";
 import reseticon from "@/assets/auth/forgot/reset.svg"
 import Lock from "@/assets/auth/icons/lock.svg";
 import Hidden from "@/assets/auth/icons/hidden.svg";
+import { useFormik } from "formik";
+import { resetPassSchema } from "@/assets/yup/schema";
 
 const ResetPassword = ({ width }) => {
   const [hidden, setHidden] = useState(true);
+
+  const formik = useFormik({
+    initialValues: {
+      password: '',
+      confirmPassword: '',
+    },
+    validationSchema: resetPassSchema,
+    onSubmit: (values) => {
+      console.log('Password:', values.password);
+    },
+  });
+
 
   return (
     <div className="relative px-10 py-12 bg-white  rounded-xl" style={{ width }}>
@@ -20,50 +34,96 @@ const ResetPassword = ({ width }) => {
         <span className="flex items-center justify-center">
           <p className="text-[14px]">Set a new password to enable you continue with your login process</p>
         </span>
-        <div className="gap-5 mt-10">
-          <div className="flex items-center justify-between gap-3 px-4 overflow-hidden bg-white  rounded-xl border custom-input my-5">
-            <div className="flex w-[90%] gap-3">
-              <Image src={Lock} width={15} alt="user-icon" />
-              <input
-                type={hidden ? "password" : "text"}
-                placeholder="Password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-                name="password"
-                className="py-3 outline-none text-[16px] w-full"
-              />
+        <form onSubmit={formik.handleSubmit}>
+          <div className="gap-5 mt-10">
+            <div className="flex flex-col overflow-hidden bg-white custom-input my-5">
+              <div className="flex items-center justify-between gap-3 px-4 border rounded-xl">
+                <div className="flex w-[90%] gap-3">
+                  <input
+                    type={hidden ? "password" : "text"}
+                    placeholder="Password"
+                    id="password"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.password}
+                    name="password"
+                    className="py-3 outline-none text-[16px] w-full"
+                  />
+                  <Image
+                    src={hidden ? Hidden : Show}
+                    width={15}
+                    alt="hidden-icon"
+                    onClick={() => setHidden(!hidden)}
+                    className="cursor-pointer"
+                  />
+                </div>
+              </div>
+              {formik.touched.password && formik.errors.password ? (
+                <div className="text-red-500 text-[12px] mt-1">
+                  {formik.errors.password}
+                </div>
+              ) : null}
             </div>
-            <Image
-              src={hidden ? Hidden : Show}
-              width={15}
-              alt="hidden-icon"
-              onClick={() => setHidden(!hidden)}
-              className="cursor-pointer"
-            />
-          </div>
 
-          <div className="flex items-center justify-between gap-3 px-4 overflow-hidden bg-white border  rounded-xl custom-input">
-            <div className="flex w-[90%] gap-3">
-              <Image src={Lock} width={15} alt="user-icon" />
-              <input
-                type={hidden ? "password" : "text"}
-                placeholder="Password"
-                id="password"
-                onChange={(e) => setPassword(e.target.value)}
-                name="password"
-                className="py-3 outline-none text-[16px] w-full"
-              />
+            <div className="flex flex-col overflow-hidden bg-white custom-input">
+              <div className="flex items-center justify-between gap-3 px-4 border rounded-xl">
+                <div className="flex w-[90%] gap-3">
+                  <input
+                    type={hidden ? "password" : "text"}
+                    placeholder="Confirm Password"
+                    id="confirmPassword"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.confirmPassword}
+                    name="confirmPassword"
+                    className="py-3 outline-none text-[16px] w-full"
+                  />
+                  <Image
+                    src={hidden ? Hidden : Show}
+                    width={15}
+                    alt="hidden-icon"
+                    onClick={() => setHidden(!hidden)}
+                    className="cursor-pointer"
+                  />
+                </div>
+              </div>
+              {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                <div className="text-red-500 text-[12px] mt-1">
+                  {formik.errors.confirmPassword}
+                </div>
+              ) : null}
             </div>
-            <Image
-              src={hidden ? Hidden : Show}
-              width={15}
-              alt="hidden-icon"
-              onClick={() => setHidden(!hidden)}
-              className="cursor-pointer"
-            />
-          </div>
-        </div>
 
+            {/* <div className="flex items-center justify-between gap-3 px-4 overflow-hidden bg-white border  rounded-xl custom-input">
+              <div className="flex flex-col overflow-hidden bg-white custom-input">
+                <div className="flex w-[90%] gap-3">
+                  <input
+                    type={hidden ? "password" : "text"}
+                    placeholder="Confirm Password"
+                    id="confirmPassword"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.confirmPassword}
+                    name="confirmPassword"
+                    className="py-3 outline-none text-[16px] w-full"
+                  />
+                  <Image
+                    src={hidden ? Hidden : Show}
+                    width={15}
+                    alt="hidden-icon"
+                    onClick={() => setHidden(!hidden)}
+                    className="cursor-pointer"
+                  />
+                </div>
+              </div>
+              {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                <div className="text-red-500 text-[12px] mt-1">
+                  {formik.errors.confirmPassword}
+                </div>
+              ) : null}
+            </div> */}
+          </div>
+        </form>
         <div className="flex flex-col gap-5">
           <button className="text-white bg-dark-orange rounded-xl text-[16px] my-4 p-3" >
             Submit

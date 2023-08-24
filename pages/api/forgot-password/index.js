5aa73e58432589632a64ab0605058ffbd813be0c
adnanali4131 <adnanali4131@gmail.com>
2023-08-24 12:20:59 +0500

    forget & reset password

--- pages/api/forgot-password/index.js
+++ pages/api/forgot-password/index.js
@@ -1,8 +1,11 @@
+import { sendEmail } from "@/aws/ses";
+import prisma from "@/db";
+import jwt from "jsonwebtoken";
 
 export default async function forgotPasswordHandler(req, res) {
   try {
     if (req.method === "POST") {
-      const { email } = JSON.parse(req.body);
+      const { email } = req.body;
 
       const user = await prisma.user.findUnique({
         where: {
@@ -14,15 +17,25 @@ export default async function forgotPasswordHandler(req, res) {
         return res.status(404).json({ message: "Email not found" });
       }
 
-
       const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
         expiresIn: "1h",
       });
 
+      const subject = "Reset Password";
+      const htmlTemplate = `
+              <p>Click on the link below to reset your password:</p>
+              <a href="${`${process.env.RESET_URL}?token=${token}`}">Reset Password</a>
+            `;
 
+      await sendEmail({
+        recipientEmail: user.email,
+        subject: subject,
+        htmlTemplate: htmlTemplate
+      });
 
       return res.json({ message: "Reset password link has been sent to your email" });
     }
+
   } catch (error) {
     return res
       .status(500)
@@ -31,35 +44,3 @@ export default async function forgotPasswordHandler(req, res) {
 }
 
 
-export default async function resetPasswordHandler(req, res) {
-  try {
-    if (req.method === "POST") {
-      const { token, newPassword } = JSON.parse(req.body);
-
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      const user = await prisma.user.findUnique({
-        where: {
-          id: decoded.userId,
-        },
-      });
-
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-      await prisma.user.update({
-        where: { id: decoded.userId },
-        data: { password: hashedPassword },
-      });
-
-      return res.json({ message: "Password has been reset successfully" });
-    }
-  } catch (error) {
-    return res
-      .status(500)
-      .json({ message: "Internal Server Error", error: error.message });
-  }
-}
